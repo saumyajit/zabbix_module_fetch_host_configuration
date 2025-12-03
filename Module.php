b724b2476d8f083e3a219b319949459921cc9038
Saumyajit <saumyajit@users.noreply.github.com>
2025-12-03 15:35:55 +0530

    Add Module class for HostConfig module

---
+++ Module.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types = 1);
+ 
+namespace Modules\HostConfig;
+ 
+use APP;
+use CController as CAction;
+use Zabbix\Core\CModule;
+ 
+/**
+ * Please see Core\CModule class for additional reference.
+ */
+class Module extends CModule {
+ 
+	/**
+	 * Initialize module.
+	 */
+	public function init(): void {
+		// Initialize main menu (CMenu class instance).
+		APP::Component()->get('menu.main')
+			->findOrAdd(_('Monitoring'))
+				->getSubmenu()
+					->insertAfter(_('Discovery'),((new \CMenuItem(_('Host Configuration (RO)')))
+						->setAction('gethostro.view'))
+					);
+	}
+ 
+	/**
+	 * Event handler, triggered before executing the action.
+	 *
+	 * @param CAction $action  Action instance responsible for current request.
+	 */
+	public function onBeforeAction(CAction $action): void {
+	}
+ 
+	/**
+	 * Event handler, triggered on application exit.
+	 *
+	 * @param CAction $action  Action instance responsible for current request.
+	 */
+	public function onTerminate(CAction $action): void {
+	}
+}

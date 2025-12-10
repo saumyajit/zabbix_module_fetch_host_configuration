e8ff46da40509eca63b329ca763bbbc643c8b84a
Saumyajit <saumyajit@users.noreply.github.com>
2025-12-10 19:23:51 +0530

    Update viewhost.php

--- views/viewhost.php
+++ views/viewhost.php
@@ -234,7 +234,7 @@ if (isset($_POST['host'])) {
 			'selectHostDiscovery' => array('parent_hostid','host'),
 			'selectInterfaces' => array('interfaceid','type','main','available','error','details','ip','dns','port','useip'),
 //			'selectInventory' => array('type','type_full','name','alias','os','os_full','os_short','serialno_a','serialno_b','tag','asset_tag','macaddress_a','macaddress_b','hardware,hardware_full','software,software_full','software_app_a','software_app_b','software_app_c','software_app_d','software_app_e','contact','location','location_lat','location_lon','notes','chassis','model','hw_arch','vendor','contract_number','installer_name','deployment_status','url_a','url_b','url_c','host_networks','host_netmask','host_router','oob_ip','oob_netmask','oob_router','date_hw_purchase','date_hw_install','date_hw_expiry','date_hw_decomm','site_address_a','site_address_b','site_address_c','site_city','site_state','site_country','site_zip','site_rack','site_notes','poc_1_name','poc_1_email','poc_1_phone_a','poc_1_phone_b','poc_1_cell','poc_1_screen','poc_1_notes','poc_2_name','poc_2_email','poc_2_phone_a','poc_2_phone_b','poc_2_cell','poc_2_screen','poc_2_notes'),
-			'selectInventory' => array('type','type_full','name','os','os_short','contact'),
+			'selectInventory' => array('type','type_full','name','os','contact'),
 			'selectMacros' => array('hostmacroid','macro','value','description','type','automatic'),
 			'selectParentTemplates' => array('templateid','name','link_type','uuid'),
 			'selectTags' => array('tag','value','automatic')
@@ -435,7 +435,6 @@ $inventory_labels = [
     'type_full'  => 'Product',
     'name'       => 'Host Name',
     'os'         => 'OS',
-    'os_short'   => 'OS (Short)',
     'contact'    => 'Customer'
 ];
 
@@ -444,8 +443,7 @@ $inventory_order = [
     'contact',
     'type_full',
     'type',
-    'os',
-    'os_short'
+    'os'
 ];
 
 $inventory_raw = $hostInfo[0]['inventory'];
